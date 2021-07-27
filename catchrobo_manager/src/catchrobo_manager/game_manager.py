aa432cd5d98ce6490c4fa43869fedbcc9e7ba4b4
paris <paris@example.com>
2021-07-27 17:10:05 +0900

    go ok

--- catchrobo_manager/src/catchrobo_manager/game_manager.py
+++ catchrobo_manager/src/catchrobo_manager/game_manager.py
@@ -43,6 +43,19 @@ class GameManager(object):
 
         self.compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
 
+        self._pose_stamped = PoseStamped()
+        self._pose_stamped.header.frame_id ="/world" # Hard coded for now
+        
+        # Create a moveit ik request
+        self._ik_request = PositionIKRequest() 
+        self._ik_request.group_name = 'arm0' # Hard coded for now
+        self._ik_request.timeout.secs = 0.1
+        self._ik_request.avoid_collisions = True 
+        self._ik_request.attempts= 100
+
+        self._pick_quat = tf.transformations.quaternion_from_euler(np.pi, 0,-np.pi/2)
+        
+
     def readCsvs(self):
         rospack = rospkg.RosPack()
         # get the file path for rospy_tutorials
@@ -57,10 +70,10 @@ class GameManager(object):
         shoot_csv = config_path +self._color+ "_shoot.csv"
         self._shoots =  ObjectDatabase(shoot_csv, "open")
     
-    # def checkGoCommon(self):
-    #     if not self._can_go_common:
-    #         self._can_go_common = self._box[self._box["open"]]["priority"].max() <= - 3  #0,1,2が埋まればcommonに入ってよい
-    #     return self._can_go_common
+    def checkGoCommon(self):
+        if not self._can_go_common:
+            self._can_go_common = self._box[self._box["open"]]["priority"].max() <= - 3  #0,1,2が埋まればcommonに入ってよい
+        return self._can_go_common
 
     #[TODO]
     def mannualAction(self):
@@ -79,11 +92,8 @@ class GameManager(object):
         # target_pose.position.y -= 0.05
         target_pose.position.z += above_val
         
-        # target_pose.position =Point(0.307048603838,0.665251814232, 0.238604612133)
-        quat = tf.transformations.quaternion_from_euler(np.pi, 0,-np.pi/2)
-        target_pose.orientation = Quaternion(*quat)
-        # target_pose.orientation = Quaternion( -0.247424555486, -0.47564766276,  -0.0118072900308, 0.84403849329)
-        rospy.loginfo(target_pose)
+        target_pose.orientation = Quaternion(*self._pick_quat)
+        rospy.loginfo(target_pose.position)
         ret = self.go(target_pose, mode)
         # self._arm.stop()
         
@@ -91,66 +101,60 @@ class GameManager(object):
     
 
     def go(self, goal, mode):
-        if mode == "free":
-            pose_stamped = PoseStamped()
-            pose_stamped.header.frame_id ="/world" # Hard coded for now
-            pose_stamped.header.stamp = rospy.Time.now()
-            pose_stamped.pose = goal 
-            
-            # Create a moveit ik request
-            ik_request = PositionIKRequest() 
-            ik_request.group_name = 'arm0' # Hard coded for now
-            ik_request.pose_stamped = pose_stamped
-            ik_request.timeout.secs = 0.1
-            ik_request.avoid_collisions = True 
-            
-            request_value = self.compute_ik(ik_request)
 
-            rospy.loginfo(request_value)
-            rospy.sleep(1)
-
-            
-            # goal = 
-            plan = RobotTrajectory()
-            plan.joint_trajectory.header.frame_id = "/world"
-            plan.joint_trajectory.joint_names =  ["arm/joint1", "arm/joint2","arm/joint3", "arm/joint4", "arm/joint5"]
-            
-            point = JointTrajectoryPoint()
-            point.positions = request_value.solution.joint_state.position
-
-            current_point = JointTrajectoryPoint()
-            current_point.positions = self._arm.get_current_joint_values()
-            plan.joint_trajectory.points = [ current_point, point]
-
-            ret = self._arm.execute(plan, wait=True)
+        now =  rospy.Time.now()
+        self._pose_stamped.header.stamp = now
+        self._pose_stamped.pose = goal 
+        self._ik_request.pose_stamped = self._pose_stamped
+        self._ik_request.robot_state = self._robot.get_current_state()
         
-        elif mode == "safe":
-            # now =  rospy.Time.now().to_sec()
-            # pose_stamped = PoseStamped()
-            # pose_stamped.header.frame_id ="/world" # Hard coded for now
-            # pose_stamped.header.stamp = rospy.Time.now()
-            # pose_stamped.pose = goal 
-            
-            # # Create a moveit ik request
-            # ik_request = PositionIKRequest() 
-            # ik_request.group_name = 'arm0' # Hard coded for now
-            # ik_request.pose_stamped = pose_stamped
-            # ik_request.timeout.secs = 0.1
-            # ik_request.avoid_collisions = False 
-            
-            
-            # request_value = self.compute_ik(ik_request)
-            # # rospy.loginfo(request_value)
+        request_value = self.compute_ik(self._ik_request)
+        if request_value.error_code.val <0:
+            return  False
+        joints = request_value.solution.joint_state.position
+        # rospy.loginfo(joints)
+        
+        self._arm.set_joint_value_target(joints[0:5])
+        dt = rospy.Time.now().to_sec() - now.to_sec()
+        rospy.loginfo("IK time : {}".format(dt))
 
-            # joints = request_value.solution.joint_state.position
-            # rospy.loginfo(joints)
-            # rospy.loginfo(request_value.error_code)
-            # self._arm.set_joint_value_target(joints[0:5])
-            # dt = rospy.Time.now().to_sec() - now
-            # temp  = "IK time : {}".format(dt)
-            # rospy.loginfo(temp)
+        # if mode == "free":
+        #     pose_stamped = PoseStamped()
+        #     pose_stamped.header.frame_id ="/world" # Hard coded for now
+        #     pose_stamped.header.stamp = rospy.Time.now()
+        #     pose_stamped.pose = goal 
+            
+        #     # Create a moveit ik request
+        #     ik_request = PositionIKRequest() 
+        #     ik_request.group_name = 'arm0' # Hard coded for now
+        #     ik_request.pose_stamped = pose_stamped
+        #     ik_request.timeout.secs = 0.1
+        #     ik_request.avoid_collisions = True 
+            
+        #     request_value = self.compute_ik(ik_request)
 
-            self._arm.set_pose_target(goal)
+        #     rospy.loginfo(request_value)
+        #     rospy.sleep(1)
+
+            
+        #     # goal = 
+        #     plan = RobotTrajectory()
+        #     plan.joint_trajectory.header.frame_id = "/world"
+        #     plan.joint_trajectory.joint_names =  ["arm/joint1", "arm/joint2","arm/joint3", "arm/joint4", "arm/joint5"]
+            
+        #     point = JointTrajectoryPoint()
+        #     point.positions = request_value.solution.joint_state.position
+
+        #     current_point = JointTrajectoryPoint()
+        #     current_point.positions = self._arm.get_current_joint_values()
+        #     plan.joint_trajectory.points = [ current_point, point]
+
+        #     ret = self._arm.execute(plan, wait=True)
+        
+        if mode == "safe":
+            
+
+            # self._arm.set_pose_target(goal)
             now =  rospy.Time.now().to_sec()
             plan = self._arm.plan()
 
@@ -175,15 +179,6 @@ class GameManager(object):
 
         return ret
 
-
-
-    def vertical_move(self, relative_m):
-        now = self._arm.get_current_pose()
-        target_pose = now.pose
-        target_pose.position.z += relative_m
-        self._arm.set_pose_target(target_pose)
-        ret = self._arm.go()
-
     def biscoAction(self):
         self._biscos.calcTargetId()
         target_id = self._biscos.getTargetId() 
@@ -192,18 +187,19 @@ class GameManager(object):
             self.mannualAction()
             self._next_action = ActionState.FINISH
         else:
-            if target_id <=20 :
-                self._next_action = ActionState.FINISH
-                return
+            # if target_id <=20 :
+            #     self._next_action = ActionState.FINISH
+            #     return
             self._biscos.updateState(target_id, False)
             if not self._biscos.getTagetObj()["my_area"]:
                 return
 
-            if self.goAboveObj(self._biscos, 0.136 + 0.06, "safe"):
+            
+            if self.goAboveObj(self._biscos, 0.136 + 0.06,self._go_mode):
                 self.hand(close=False)
-                self.goAboveObj(self._biscos, 0.136 + 0.04, "safe")
+                self.goAboveObj(self._biscos, 0.136 + 0.04, self._go_mode)
                 self.hand(close=True)
-                self.goAboveObj(self._biscos, 0.136 *2+ 0.05, "safe")
+                self.goAboveObj(self._biscos, 0.136 *2+ 0.05, self._go_mode)
                 self._next_action = ActionState.SHOOT
             else:
                 rospy.logerr("cannot make path")
@@ -218,10 +214,10 @@ class GameManager(object):
             self._next_action = ActionState.FINISH
         else:
             self._shoots.updateState(target_id, False)
-            if self.goAboveObj(self._shoots, 0.136 *2+ 0.05, "safe"):
-                self.goAboveObj(self._shoots,0.136 + 0.04, "safe")
+            if self.goAboveObj(self._shoots, 0.136 *2+ 0.05,self._go_mode):
+                self.goAboveObj(self._shoots,0.136 + 0.04, self._go_mode)
                 self.hand(close=False)
-                self.goAboveObj(self._shoots,0.136 + 0.06, "safe")
+                self.goAboveObj(self._shoots,0.136 + 0.06, self._go_mode)
                 self._next_action = ActionState.BISCO
             else:
                 rospy.logerr("cannot make path")
@@ -231,6 +227,7 @@ class GameManager(object):
         # self._arm.set_max_velocity_scaling_factor(1)
 
         self._next_action = ActionState.BISCO
+        self._go_mode = "safe"
         # rate = rospy.Rate(10)
         start_time = rospy.Time.now().to_sec()
         while not rospy.is_shutdown():
