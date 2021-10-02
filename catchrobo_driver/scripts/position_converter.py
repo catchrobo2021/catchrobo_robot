65638a0e764f3bd0664c02105c94f83529a20d83
yutakage <yuta0727leggodt@gmail.com>
2021-10-02 11:03:14 +0900

    motor to joint OK!!!

--- catchrobo_driver/scripts/position_converter.py
+++ catchrobo_driver/scripts/position_converter.py
@@ -11,7 +11,7 @@ class PositionConverter:
     def __init__(self, joint_offset, MOTOR_NUM=5):
         # reductionRatio[0~2]は各アクチュエータの減速比(J2 is 35:72)
         # reductionRatio[3]=1.0は第三アクチュエータベルトにおける、第二関節と第三関節間の減速比
-        # reductionRatio[4]=6.0は第四・五アクチュエータベルトにおける、根本と第三関節間の減速比
+        # reductionRatio[4]=2.0は第四・五アクチュエータベルトにおける、根本と第三関節間の減速比
         # reductionRatio[5]=1.0は第四・五アクチュエータベルトにおける、第三関節と第四・五関節間の減速比
         # reductionRatio[6]=1.4は傘歯車の減速比(25:35)
         self.__reductionRatio = [8.0, 16.45, 8.0, 1.0, 2.0, 1.0, 1.4] 
@@ -67,7 +67,7 @@ class PositionConverter:
         listAffectedDiff[3] = -joint_state.position[1] / self.__reductionRatio[4] - joint_state.position[2] / self.__reductionRatio[5] # 特に符号に注意 & joint_state.position[2]なのか,(joint_state.position[2]-joint_state.position[1])なのか迷う
         listAffectedDiff[4] = -joint_state.position[1] / self.__reductionRatio[4] - joint_state.position[2] / self.__reductionRatio[5] 
         joint_state_without_offset[3] = (-motor_state.position[3] + motor_state.position[4]) / 2.0 / self.__reductionRatio[4] + listAffectedDiff[3]
-        joint_state_without_offset[4] = (-motor_state.position[3] - motor_state.position[4]) / self.__reductionRatio[6] / self.__reductionRatio[6] + listAffectedDiff[4]
+        joint_state_without_offset[4] = (-motor_state.position[3] - motor_state.position[4]) / self.__reductionRatio[6] / self.__reductionRatio[4] / 2.0
         joint_state.position[3] = joint_state_without_offset[3] + self.__jointOffset[3]
         joint_state.position[4] = joint_state_without_offset[4] + self.__jointOffset[4]
         return joint_state
@@ -78,6 +78,7 @@ class PositionConverter:
         motor_state.position = [0] * self.MOTOR_NUM
         motor_state.velocity = [0] * self.MOTOR_NUM
         motor_state.effort = [0] * self.MOTOR_NUM
+        listAffectedDiff = [0.0, 0.0, 0.0, 0.0, 0.0]
         # 第一関節
         motor_state.position[0] = (-joint_state.position[0] + self.__jointOffset[0]) * self.__reductionRatio[0]
         # 第二関節
