2ba191ec59cec8d71e85ae1ffb5bb9afa8525976
yutakage <yuta0727leggodt@gmail.com>
2021-10-16 15:16:17 +0900

    scara moveit  setup

--- catchrobo_driver/scripts/position_converter.py
+++ catchrobo_driver/scripts/position_converter.py
@@ -1,24 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import rospy
 from sensor_msgs.msg import JointState
-from std_msgs.msg import Bool
-import rosparam
-import roslib
 import math
 
 class PositionConverter:
-    def __init__(self, joint_offset, MOTOR_NUM=5):
-        # reductionRatio[0~2]は各アクチュエータの減速比(J2 is 35:72)
-        # reductionRatio[3]=1.0は第三アクチュエータベルトにおける、第二関節と第三関節間の減速比
-        # reductionRatio[4]=2.0は第四・五アクチュエータベルトにおける、根本と第三関節間の減速比
-        # reductionRatio[5]=1.0は第四・五アクチュエータベルトにおける、第三関節と第四・五関節間の減速比
-        # reductionRatio[6]=1.4は傘歯車の減速比(25:35)
-        self.__reductionRatio = [8.0, 16.45, 8.0, 1.0, 2.0, 1.0, 1.4] 
+    def __init__(self, joint_offset, MOTOR_NUM=4):
         self.MOTOR_NUM = MOTOR_NUM
-        # オフセット量 (rad)
         self.__jointOffset = joint_offset
-        rospy.loginfo(self.__jointOffset)
 
     def convert_round_to_rad(self, round):
         rad = [0] * self.MOTOR_NUM
@@ -26,11 +14,71 @@ class PositionConverter:
             rad[i] = round[i] * 2 * math.pi
         return rad
 
+
     def convert_rad_to_round(self, rad):
         round = [0] * self.MOTOR_NUM
         for i in range(self.MOTOR_NUM):
             round[i] = rad[i] / (2.0 * math.pi)
         return round
+
+
+    def convert_motor_to_joint(self, motor_state_round):
+        motor_state = JointState()
+        motor_state.name = [""] * self.MOTOR_NUM
+        motor_state.position = [0] * self.MOTOR_NUM
+        motor_state.velocity = [0] * self.MOTOR_NUM
+        motor_state.effort = [0] * self.MOTOR_NUM
+        motor_state.position = self.convert_round_to_rad(motor_state_round.position)
+
+        joint_state = JointState()
+        joint_state.name = [""] * self.MOTOR_NUM
+        joint_state.position = [0] * self.MOTOR_NUM
+        joint_state.velocity = [0] * self.MOTOR_NUM
+        joint_state.effort = [0] * self.MOTOR_NUM
+
+        joint_state.position[0] = -motor_state.position[0] / 8.0 + self.__jointOffset[0]
+        joint_state.position[1] = +motor_state.position[1] / 8.0 + self.__jointOffset[1]
+        joint_state.position[2] = -motor_state.position[2] / 2.0 + self.__jointOffset[2]
+        joint_state.position[3] = +motor_state.position[3] / (2 * math.pi) * 0.008 + self.__jointOffset[3]
+
+        return joint_state
+
+
+    def convert_joint_to_motor(self, joint_state):
+        motor_state = JointState()
+        motor_state.name = [""] * self.MOTOR_NUM
+        motor_state.position = [0] * self.MOTOR_NUM
+        motor_state.velocity = [0] * self.MOTOR_NUM
+        motor_state.effort = [0] * self.MOTOR_NUM
+        
+        motor_state.position[0] = (-joint_state.position[0] + self.__jointOffset[0]) * 8.0
+        motor_state.position[1] = (+joint_state.position[1] - self.__jointOffset[1]) * 8.0
+        motor_state.position[2] = (-joint_state.position[2] + self.__jointOffset[2]) * 2.0
+        motor_state.position[4] = (+joint_state.position[3] - self.__jointOffset[3]) * (2 * math.pi) / 0.008
+        
+        motor_state_round = JointState()
+        motor_state_round.name = [""] * self.MOTOR_NUM
+        motor_state_round.position = [0] * self.MOTOR_NUM
+        motor_state_round.velocity = [0] * self.MOTOR_NUM
+        motor_state_round.effort = [0] * self.MOTOR_NUM
+        motor_state_round.position = self.convert_rad_to_round(motor_state.position)
+
+        return motor_state_round
+    
+
+'''
+# converter for verical articulated arm
+    def __init__(self, joint_offset, MOTOR_NUM=5):
+        # reductionRatio[0~2]は各アクチュエータの減速比(J2 is 35:72)
+        # reductionRatio[3]=1.0は第三アクチュエータベルトにおける、第二関節と第三関節間の減速比
+        # reductionRatio[4]=2.0は第四・五アクチュエータベルトにおける、根本と第三関節間の減速比
+        # reductionRatio[5]=1.0は第四・五アクチュエータベルトにおける、第三関節と第四・五関節間の減速比
+        # reductionRatio[6]=1.4は傘歯車の減速比(25:35)
+        #self.__reductionRatio = [8.0, 16.45, 8.0, 1.0, 2.0, 1.0, 1.4] #vertical articulated arm
+        self.MOTOR_NUM = MOTOR_NUM
+        # オフセット量 (rad)
+        self.__jointOffset = joint_offset
+        rospy.loginfo(self.__jointOffset)
 
     def convert_motor_to_joint(self, motor_state_rad):
         # 回転位相情報を角度情報に変換
@@ -108,5 +156,5 @@ class PositionConverter:
         motor_state_round.effort = [0] * self.MOTOR_NUM
         motor_state_round.position = self.convert_rad_to_round(motor_state.position)
         return motor_state_round
-
+'''
 
