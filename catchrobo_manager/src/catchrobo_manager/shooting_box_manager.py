42575926fa260738f74895a06aeac03391783ac7
Tg-Inoue <wellup@fuji.waseda.jp>
2021-11-21 09:38:25 +0900

    not perdect but useful shoot algorism

--- catchrobo_manager/src/catchrobo_manager/shooting_box_manager.py
+++ catchrobo_manager/src/catchrobo_manager/shooting_box_manager.py
@@ -47,6 +47,8 @@ class ShootingBoxManager():
     def canGoCommon(self):
         group = self._objects.groupby("sorter_id").sum()
         in_sorter_bool = group["exist"] >= 1
+        # if self._objects.loc[4, "exist"] == 4 or self._objects.loc[5, "exist"] == 4:
+        #    return in_sorter_bool.sum() >= 8
         return in_sorter_bool.sum() >= 3
 
     def shoot(self, id):
@@ -64,6 +66,193 @@ class ShootingBoxManager():
         box1 = box1_l+box1_r
         box2 = box2_l+box2_r
         box3 = box3_l+box3_r
+        # box1 (l r) box2(l r) box3(l r)
+
+        target1 = 1
+        target2 = 3
+        is_settarget1 = 0
+
+        if self.canGoCommon() == 1:  # yes common
+            '''
+            if box3_l == 4 and ((box1_l == 1 or box1_r == 1) and (box2_l == 1 or box2_r == 1)):  # ????x?
+                is_settarget1 = 0
+                target1 = 5
+                if box1_r == 0:
+                    target2 = 1
+                elif box2_l == 0:
+                    target2 = 2
+                elif box2_r == 0:
+                    target2 = 3
+                elif box3_l == 0:
+                    target2 = 4
+                elif box1_l == 0:  # 0?????
+                    target2 = 0
+                elif box3_r == 0:
+                    target2 = 5
+                self._target_ids = [target1, target2]
+
+            elif box3_r == 4 and ((box1_l == 1 or box1_r == 1) and (box2_l == 1 or box2_r == 1)):  # ?????x
+                is_settarget1 = 0
+                target1 = 4
+                if box1_r == 0:
+                    target2 = 1
+                elif box2_l == 0:
+                    target2 = 2
+                elif box2_r == 0:
+                    target2 = 3
+                elif box3_l == 0:
+                    target2 = 4
+                elif box1_l == 0:  # 0?????
+                    target2 = 0
+                elif box3_r == 0:
+                    target2 = 5
+                self._target_ids = [target1, target2]
+            '''
+            if box1 <= 6:
+                if box1_l > box1_r:
+                    self._target_ids = [1, 0]
+                    if box1_l-box1_r == 2:
+                        self._target_ids = [1, 1]
+                elif box1_l <= box1_r:
+                    self._target_ids = [0, 1]
+                    if box1_r-box1_l == 2:
+                        self._target_ids = [0, 0]
+            elif box1 == 7:
+                if box1_l > box1_r:
+                    self._target_ids = [1, 3]
+                elif box1_l <= box1_r:
+                    self._target_ids = [0, 3]
+            elif box2 <= 6:
+                if box2_l > box2_r:
+                    self._target_ids = [3, 2]
+                    if box2_l-box2_r == 2:
+                        self._target_ids = [3, 3]
+                elif box2_l <= box2_r:
+                    self._target_ids = [2, 3]
+                    if box2_r-box2_l == 2:
+                        self._target_ids = [2, 2]
+            elif box2 == 7:
+                if box2_l > box2_r:
+                    self._target_ids = [3, 5]
+                elif box2_l <= box2_r:
+                    self._target_ids = [2, 5]
+            elif box3 <= 6:
+                if box3_l > box3_r:
+                    self._target_ids = [5, 4]
+                    if box3_l-box3_r == 3:
+                        self._target_ids = [5, 5]
+                elif box3_l <= box3_r:
+                    self._target_ids = [4, 5]
+                    if box3_r-box3_l == 3:
+                        self._target_ids = [4, 4]
+            elif box3 == 7:
+                if box3_l > box3_r:
+                    self._target_ids = [5, 5]
+                elif box3_l <= box3_r:
+                    self._target_ids = [4, 4]
+
+        elif self.canGoCommon() == 0:  # no common
+
+            if box1_r == 0:  # ?0????
+                target1 = 1
+                is_settarget1 = 1
+
+            if box1_l == 0 and box1_r >= 2:  # 0?????
+                if is_settarget1 == 0:
+                    target1 = 0
+                    is_settarget1 = 1
+
+            if box2_l == 0:  # ??0???
+                if is_settarget1 == 0:
+                    target1 = 2
+                    is_settarget1 = 2
+                elif is_settarget1 == 1:
+                    target2 = 2
+                    is_settarget1 = 3
+
+            if box2_r == 0 and box2_l >= 2:  # ???0??
+                if is_settarget1 == 0:
+                    target1 = 3
+                    is_settarget1 = 2
+                elif is_settarget1 == 1:
+                    target2 = 3
+                    is_settarget1 = 3
+
+            if box3_l == 0:  # ????0?
+                if is_settarget1 == 0:
+                    target1 = 4
+                    is_settarget1 = 4
+                if is_settarget1 == (1 or 2):
+                    target2 = 4
+
+            if box3_r == 0 and box3_l >= 2:  # ????0?
+                if is_settarget1 == 0:
+                    target1 = 5
+                    is_settarget1 = 4
+                if is_settarget1 == (1 or 2):
+                    target2 = 5
+
+            if box3_l == 4 and ((box1_l == 1 or box1_r == 1) and (box2_l == 1 or box2_r == 1)):  # ????x?
+                is_settarget1 = 0
+                target1 = 5
+                if box1_r == 0:
+                    target2 = 1
+                elif box2_l == 0:
+                    target2 = 2
+                elif box2_r == 0:
+                    target2 = 3
+                elif box3_l == 0:
+                    target2 = 4
+                elif box1_l == 0:  # 0?????
+                    target2 = 0
+                elif box3_r == 0:
+                    target2 = 5
+
+            if box3_r == 4 and ((box1_l == 1 or box1_r == 1) and (box2_l == 1 or box2_r == 1)):  # ?????x
+                is_settarget1 = 0
+                target1 = 4
+                if box1_r == 0:
+                    target2 = 1
+                elif box2_l == 0:
+                    target2 = 2
+                elif box2_r == 0:
+                    target2 = 3
+                elif box3_l == 0:
+                    target2 = 4
+                elif box1_l == 0:  # 0?????
+                    target2 = 0
+                elif box3_r == 0:
+                    target2 = 5
+
+            if is_settarget1 == (1 or 2):
+                if box1_r == 0:
+                    target2 = 1
+                elif box2_l == 0:
+                    target2 = 2
+                elif box2_r == 0:
+                    target2 = 3
+                elif box3_l == 0:
+                    target2 = 4
+
+            if is_settarget1 == 4:
+                if box1_l == 0:  # 0?????
+                    target2 = 0
+                elif box3_r == 0:
+                    target2 = 5
+
+            self._target_ids = [target1, target2]
+
+        '''
+        box1_l = self._objects.loc[0, "exist"]
+        box1_r = self._objects.loc[1, "exist"]
+        box2_l = self._objects.loc[2, "exist"]
+        box2_r = self._objects.loc[3, "exist"]
+        box3_l = self._objects.loc[4, "exist"]
+        box3_r = self._objects.loc[5, "exist"]
+        box1 = box1_l+box1_r
+        box2 = box2_l+box2_r
+        box3 = box3_l+box3_r
+        # box1 (l r) box2(l r) box3(l r) 
 
         val = 100000*box1_l+10000*box1_r+1000*box2_l + 100*box2_r+10*box3_l+box3_r
         if val == 0:
@@ -87,6 +276,24 @@ class ShootingBoxManager():
         elif val == 1010:
             # 001010
             self._target_ids = [0, 1]
+        elif val >= 1 and val <= 44:
+            # 0000??
+            self._target_ids = [0, 2]
+        elif int(val/100) >= 1 and int(val/100) <= 44:
+            # 00??00
+            self._target_ids = [0, 4]
+            if val >= 1 and val <=44:
+            # 00!!??
+                self._target_ids = [0, 1]
+        elif int(val/10000)>= 1 and int(val/10000) <=44:
+            # ??0000
+            self._target_ids = [2, 4]
+            if val >= 1 and val <=44:
+            # !!00??
+                self._target_ids = [2,3]
+            elif int(val/100)>= 1 and int(val/100) <=44:
+            # !!??00
+                self._target_ids = [4,5]
         else:
             if box1 <= 6:
                 if box1_l > box1_r:
@@ -130,6 +337,7 @@ class ShootingBoxManager():
                     self._target_ids = [5, 5]
                 elif box3_l <= box3_r:
                     self._target_ids = [4, 4]
+    '''
 
     def getTargetTwin(self):
         return [self.getObj(id) for id in self._target_ids], None
